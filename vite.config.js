e864eae5aa41ac53d84b8da1963df39649df2279
Fakhrul-17 <muhamadfakhrularifin17@gmail.com>
2026-07-18 01:39:53 +0700

    Final: Mission Advanced Front-End 2 Complete

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Mission-Advanced-Front-End-1/'
+  base: '/Mission-Advanced-Front-End-2/'
 })
